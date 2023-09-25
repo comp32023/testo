2d90e0d68f83e6083a2b7228e0cd8ce6bd7e3fd4
comp32023 <comp32023@mail.ru>
2023-09-25 23:15:35 +0300

    save2

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ def red():
 
 btns = ttk.Button(text='Покраска в красный', command=red) #Кнопкав нижнем левом углу
 btns.place(relx=.0, rely=1, anchor='sw', width=120, height=25)
+
+
+# Вторая кнопка
+def purple():
+    styles = ttk.Style()
+    styles.configure("BW.TLabel", background="purple")
+    btn['style'] = "BW.TLabel"
+
+btn = ttk.Button(text='Покраска в фиолетовый', command=purple) #Кнопка внизу центра
+btn.place(relx=0.5, rely=1, anchor='s', width=120, height=25)
