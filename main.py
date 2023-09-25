560ed2d077b96dfabe6f69ce4c7ab0cedf7ca387
comp32023 <comp32023@mail.ru>
2023-09-25 23:34:31 +0300

    3knopka

--- main.py
+++ main.py
@@ -19,3 +19,10 @@ def purple():
 
 btn = ttk.Button(text='Покраска в фиолетовый', command=purple) #Кнопка внизу центра
 btn.place(relx=0.5, rely=1, anchor='s', width=120, height=25)
+
+
+# Третья кнопка кнопка :3
+def black():
+    style = ttk.Style()
+    style.configure("BW.TLabel", foreground="white", background="black")
+    buttonTTK['style'] = "BW.TLabel"
